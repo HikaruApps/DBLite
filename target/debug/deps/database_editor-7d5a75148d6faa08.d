2914228c8b9c09b976d34cba73900f77ef1e9f02
Hikaru.exe <187906517+HikaruApps@users.noreply.github.com>
2025-10-22 20:47:18 +0300

    first commit

---
+++ target/debug/deps/database_editor-7d5a75148d6faa08.d
@@ -0,0 +1,5 @@
+E:\ProjectRust\database_editor\target\debug\deps\database_editor-7d5a75148d6faa08.d: src\main.rs
+
+E:\ProjectRust\database_editor\target\debug\deps\libdatabase_editor-7d5a75148d6faa08.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/database_editor-c4ced44bbc197310.d
@@ -0,0 +1,5 @@
+E:\ProjectRust\database_editor\target\debug\deps\database_editor-c4ced44bbc197310.d: src\main.rs
+
+E:\ProjectRust\database_editor\target\debug\deps\libdatabase_editor-c4ced44bbc197310.rmeta: src\main.rs
+
+src\main.rs:

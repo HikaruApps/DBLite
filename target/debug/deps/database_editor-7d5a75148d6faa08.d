8f27fbced8ac8bf9f17c2ac544a50f90e7467dfa
Hikaru.exe <187906517+HikaruApps@users.noreply.github.com>
2025-10-22 20:47:50 +0300

    Delete target directory

--- target/debug/deps/database_editor-7d5a75148d6faa08.d
+++
@@ -1,5 +0,0 @@
-E:\ProjectRust\database_editor\target\debug\deps\database_editor-7d5a75148d6faa08.d: src\main.rs
-
-E:\ProjectRust\database_editor\target\debug\deps\libdatabase_editor-7d5a75148d6faa08.rmeta: src\main.rs
-
-src\main.rs:
--- target/debug/deps/database_editor-c4ced44bbc197310.d
+++
@@ -1,5 +0,0 @@
-E:\ProjectRust\database_editor\target\debug\deps\database_editor-c4ced44bbc197310.d: src\main.rs
-
-E:\ProjectRust\database_editor\target\debug\deps\libdatabase_editor-c4ced44bbc197310.rmeta: src\main.rs
-
-src\main.rs:

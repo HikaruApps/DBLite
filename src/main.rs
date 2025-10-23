586cd835d0781e42f1cbfbe3a050383985008976
Hikaru.exe <187906517+HikaruApps@users.noreply.github.com>
2025-10-23 22:19:39 +0300

    main menu added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
 fn main() {
-    
+
+    let mut help = String::new();
+    println!("Hi, I'll help you with databases. To see all the commands, enter - .help");
+
+    io::stdin()
+    .read_line(&mut help)
+    .expect("Failed to read line");
+       
+       let help = help.trim();
+       
+    if help == ".help" {
+        println!("1. .add -> Create DB file");
+        println!("2. .write -> Write a DB file");
+    } else {
+        println!("Unable to read line. For a list of commands, use -> .help");
+    }
 }

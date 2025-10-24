ead27bfcbf041d90fe7fad9eaf121fd1ca9fe13a
Hikaru.exe <187906517+HikaruApps@users.noreply.github.com>
2025-10-24 17:38:28 +0300

    Minor bug fixes and main menu improvements
    A loop has been added to prevent the program from terminating after an incorrectly entered command. Added .exit command to exit the program

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 use std::io;
 fn main() {
 
-    let mut help = String::new();
     println!("Hi, I'll help you with databases. To see all the commands, enter - .help");
+
+    loop {
+    let mut help = String::new();
 
     io::stdin()
     .read_line(&mut help)
     .expect("Failed to read line");
        
-       let help = help.trim();
-       
-    if help == ".help" {
+    if help.trim() == ".help" {
         println!("1. .add -> Create DB file");
         println!("2. .write -> Write a DB file");
+        println!("3. .exit -> Exit the program")
+        
     } else {
         println!("Unable to read line. For a list of commands, use -> .help");
     }
+    if help.trim() == ".exit" {
+        println!("The program ends");
+        break;
+    }
+}
 }

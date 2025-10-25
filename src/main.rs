556a213405261644ea4eb461934002883206ef0a
Hikaru.exe <187906517+HikaruApps@users.noreply.github.com>
2025-10-25 20:05:48 +0300

    The main menu has been optimized and new features have been added.
    The code has been optimized, the .add() & .write functions have been added, and minor bugs have been fixed.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,90 @@
-use std::io;
+use std::io::{self, Write};
+use std::fs::{self, File};
+use std::num::ParseIntError;
+use std::path;
+use std::path::Path;
+use std::path::PathBuf;
+use std::process::Command;
 fn main() {
 
     println!("Hi, I'll help you with databases. To see all the commands, enter - .help");
 
     loop {
-    let mut help = String::new();
-
-    io::stdin()
-    .read_line(&mut help)
-    .expect("Failed to read line");
-       
-    if help.trim() == ".help" {
-        println!("1. .add -> Create DB file");
-        println!("2. .write -> Write a DB file");
-        println!("3. .exit -> Exit the program")
-        
-    } else {
-        println!("Unable to read line. For a list of commands, use -> .help");
+    let mut command = String::new();
+    if io::stdin()
+    .read_line(&mut command)
+    .is_err() {
+        eprintln!("Failed to read line");
+        continue;
     }
-    if help.trim() == ".exit" {
+       
+  match command.trim() {
+      ".help" => {
+                println!("1. .add   -> Create folder for DB (enter folder path)");
+                println!("2. .write -> Create a DB file (enter file path)");
+                println!("3. .exit  -> Exit the program");
+                println!("Created by Hikaru.exe");
+                println!("GitHub -> https://github.com/HikaruApps");
+      }
+      ".add" => {
+        if let Err(e) = add() {
+            eprintln!("Error in .add: {}", e);
+        }
+      }
+      ".write" => {
+        if let Err(e) = write_db() {
+            eprintln!("Error in .write: {}", e);
+        }
+      }
+      ".exit" => {
         println!("The program ends");
         break;
-    }
+      }
+      other => {
+        println!("Unknow command: '{}'. For a list of commands, use => .help", other);
+      }
+  }
 }
+}
+
+fn add() -> io::Result<()> {
+
+    println!("Specify the path to create the file");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input); 
+    let folder_path= input.trim();
+
+    let path = Path::new(folder_path);
+
+    if path.exists() {
+        println!("This folder already exists: {}", folder_path);
+    } else {
+        fs::create_dir_all(path)?;
+        println!("The folder named <{}> has been successfully created.", folder_path);
+    }
+
+    Ok(())
+}
+
+fn write_db() -> io::Result<()> {
+    println!("Specify the file path to create the DB file (e.g. E:\\path\\db.txt):");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    let file_path = input.trim();
+
+    let path = PathBuf::from(file_path);
+
+    if let Some(parent) = path.parent() {
+        if !parent.exists() {
+            fs::create_dir_all(parent)?;
+        }
+    }
+
+    let mut file = File::create(&path)?;
+    writeln!(file, "#DBLite\n# created by Hikaru.exe")?;
+
+    println!("File created: {}", path.display());
+    Ok(())
 }
